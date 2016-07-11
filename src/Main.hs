f8b2cbdf012e508f92d475bcb4bc929a8d57035d
Bobby Priambodo <bobby.priambodo@gmail.com>
2016-07-11 16:00:37 +0700

    Improve program structure

--- src/Main.hs
+++ src/Main.hs
@@ -2,22 +2,41 @@
 
 module Main where
 
+import           Data.ByteString
 import           Data.Time.Clock
 import           Network.HTTP.Client
 import           Network.HTTP.Conduit
 import           Network.HTTP.Simple
 
+buildAuthRequest :: ByteString -> ByteString -> IO Request
+buildAuthRequest username password = do
+  authRequest <- parseRequest "POST https://academic.ui.ac.id/main/Authentication/Index"
+  return (setRequestSecure True
+    $ setRequestBodyURLEncoded [("u", username), ("p", password)] authRequest)
+
+buildChangeRoleRequest :: IO Request
+buildChangeRoleRequest = do
+  changeRoleRequest <- parseRequest "GET https://academic.ui.ac.id/main/Authentication/ChangeRole"
+  return (setRequestSecure True changeRoleRequest)
+
+
 main :: IO ()
 main = do
-  authReq' <- parseRequest "POST https://academic.ui.ac.id/main/Authentication/Index"
-  let authReq = setRequestSecure True
-              $ setRequestBodyURLEncoded [("u", "widyanto.bagus"), ("p", "lala123")] authReq'
-  authResp <- httpLBS authReq
+  -- Make authentication request
+  authRequest <- buildAuthRequest "widyanto.bagus" "lala123"
+  authResponse <- httpLBS authRequest
+
+  -- Create cookie jar with authentication cookies from auth request
   now <- getCurrentTime
-  let newJar = fst $ updateCookieJar authResp authReq now (createCookieJar [])
-  print (getResponseHeaders authResp)
-  changeRoleReq' <- parseRequest "GET https://academic.ui.ac.id/main/Authentication/ChangeRole"
-  let changeRoleReq = setRequestSecure True
-              $ authReq' { cookieJar = Just newJar }
-  changeRoleResp <- httpLBS changeRoleReq
+  let emptyJar = createCookieJar []
+  let authenticationCookieJar = fst $ updateCookieJar authResponse authRequest now emptyJar
+
+  -- Make change role request
+  changeRoleRequest <- buildChangeRoleRequest
+  changeRoleResp <- httpLBS (changeRoleRequest { cookieJar = Just authenticationCookieJar })
+
+  -- At this point, we have successfully authenticated.
+  -- Use authenticationCookieJar for further requests.
+  print (getResponseHeaders changeRoleResp)
   print (getResponseBody changeRoleResp)
+  print authenticationCookieJar
