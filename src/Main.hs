1583053b94f8a17a50cd6bf6117d94375c29672a
Bobby Priambodo <bobby.priambodo@gmail.com>
2016-09-11 23:28:41 +0700

    First attempt on getting command from cmd

--- src/Main.hs
+++ src/Main.hs
@@ -1,21 +1,65 @@
 module Main where
 
 import           Configuration.Dotenv (loadFile)
-import           System.Environment   (getEnv)
+import           System.Environment   (getArgs, getEnv)
 
 import           Auth                 (authenticate)
 
 main :: IO ()
 main = do
+  -- TODO: check if .env exist, fail if it doesn't
+
   -- Load environment variables
   loadFile False ".env"
-  username <- getEnv "SIAK_USERNAME"
-  password <- getEnv "SIAK_PASSWORD"
 
-  putStrLn $ "Authenticating with credentials of " ++ username ++ "..."
+  args <- getArgs
 
-  authenticationCookieJar <- authenticate username password
+  if null args
+    then do
+      printHelp
+      error "No command given."
+    else do
+      let command = head args
 
-  -- At this point, we have successfully authenticated.
-  -- Use authenticationCookieJar for further requests.
-  print authenticationCookieJar
+      if not (isValidCommand command)
+        then do
+          printHelp
+          error $ "Invalid command: " ++ command
+        else do
+          putStrLn $ "Running " ++ command ++ " command."
+
+          username <- getEnv "SIAK_USERNAME"
+          password <- getEnv "SIAK_PASSWORD"
+
+          putStrLn $ "Authenticating with credentials of " ++ username ++ "..."
+
+          authenticationCookieJar <- authenticate username password
+
+          -- At this point, we have successfully authenticated.
+          -- Use authenticationCookieJar for further requests.
+          case command of
+            "summary" ->
+              error "TODO: summary"
+            "grades" ->
+              error "TODO: grades"
+            "fees" ->
+              error "TODO: fees"
+
+isValidCommand :: String -> Bool
+isValidCommand command
+  | command == "summary"  = True
+  | command == "grades"   = True
+  | command == "fees"     = True
+  | otherwise             = False
+
+printHelp :: IO ()
+printHelp =
+  putStrLn $ concat
+    [ "Usage:\n"
+    , "   scraper [command]\n"
+    , "\n"
+    , "   Available commands:\n"
+    , "       summary     - Get a summary of your student status.\n"
+    , "       grades      - Get your grades for this semester.\n"
+    , "       fees        - Get your last fee payment status.\n"
+    ]
