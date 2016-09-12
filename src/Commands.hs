83c33d5e688e15920aca2390382a9c8f5c17f5db
Bobby Priambodo <bobby.priambodo@gmail.com>
2016-09-12 18:37:28 +0700

    Implement summary command

--- src/Commands.hs
+++ src/Commands.hs
@@ -1,19 +1,67 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Commands where
 
-import           Network.HTTP.Client  (parseRequest)
-import           Network.HTTP.Conduit (CookieJar, cookieJar)
-import           Network.HTTP.Simple  (httpLBS, setRequestBodyURLEncoded,
-                                       setRequestSecure)
+import qualified Data.ByteString.Lazy as B
+import qualified Data.ByteString.Lazy.Char8 as B8
+import Data.Char
+import           Network.HTTP.Client           (parseRequest, Request)
+import           Network.HTTP.Conduit          (CookieJar, cookieJar)
+import           Network.HTTP.Simple           (getResponseBody, httpLBS,
+                                                setRequestBodyURLEncoded,
+                                                setRequestSecure)
+import           Text.HTML.TagSoup
 
-import           Endpoints            (summaryUrl)
+import           Endpoints                     (summaryUrl)
 
+type Key = B.ByteString
+type Value = B.ByteString
+
+data Entry = Entry Key Value deriving (Show)
+
+buildSummaryRequest :: IO Request
 buildSummaryRequest = do
   request <- parseRequest $ "GET " ++ summaryUrl
   return (setRequestSecure True request)
+
+containsWhitespace :: B.ByteString -> Bool
+containsWhitespace = B.any (== (fromIntegral $ ord '\n'))
+
+makePairs :: [B.ByteString] -> [(B.ByteString, B.ByteString)]
+makePairs [] = []
+makePairs [_] = []
+makePairs (a:b:xs) = (a, b) : makePairs xs
+
+pairsToEntries :: [(B.ByteString, B.ByteString)] -> [Entry]
+pairsToEntries [] = []
+pairsToEntries ((a, b):xs) = Entry a b : pairsToEntries xs
+
+printEntry :: Entry -> IO ()
+printEntry (Entry key value) =
+  B8.putStrLn $ B.intercalate ": " [key, value]
+
+printEntries :: [Entry] -> IO ()
+printEntries [] = return ()
+printEntries (entry:restOfEntries) = do
+  printEntry entry
+  printEntries restOfEntries
+
+parseSummary :: B.ByteString -> [Entry]
+parseSummary html =
+  pairsToEntries $ makePairs withoutWhitespace
+  where
+    tags = parseTags html
+    summaryTable =
+          takeWhile (~/= TagComment (" Grafik IP/IPK " :: B.ByteString)) $
+          dropWhile (~/= TagComment (" Summary " :: B.ByteString)) tags
+    texts = map fromTagText $ filter isTagText summaryTable
+    withoutWhitespace = filter (not . containsWhitespace) texts
 
 showSummary :: CookieJar -> IO ()
 showSummary authenticationCookieJar = do
   request <- buildSummaryRequest
   response <- httpLBS $ request { cookieJar = Just authenticationCookieJar }
+  let responseBody = getResponseBody response
 
-  print response
+  let entries = parseSummary responseBody
+  printEntries entries
